1d152a5422df65f50774a8d857edab2ff63edd5c
Renzowit35 <Renzowitlox@gmail.com>
2025-03-23 15:23:26 +0100

    Added Black opal Added Raw Black Opal
    Added Textures/models
    
    Added Creative mode tabs for blocks and items
    
    Change version tot 0.0.2

--- src/main/java/net/renzowit35/tutorialmodrw/MCCourseMod.java
+++ src/main/java/net/renzowit35/tutorialmodrw/MCCourseMod.java
@@ -1,5 +1,8 @@
 package net.renzowit35.tutorialmodrw;
 
+import net.minecraft.world.item.CreativeModeTabs;
+import net.renzowit35.tutorialmodrw.item.ModCreativeModeTabs;
+import net.renzowit35.tutorialmodrw.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -34,6 +37,11 @@ public class MCCourseMod {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+
+
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
@@ -60,6 +68,10 @@ public class MCCourseMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BLACK_OPAL);
+            event.accept(ModItems.RAW_BLACK_OPAL);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
@@ -81,3 +93,4 @@ public class MCCourseMod {
         }
     }
 }
+
---
+++ src/main/java/net/renzowit35/tutorialmodrw/item/ModCreativeModeTabs.java
@@ -0,0 +1,46 @@
+package net.renzowit35.tutorialmodrw.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Blocks;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.renzowit35.tutorialmodrw.MCCourseMod;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MCCourseMod.MOD_ID);
+
+
+    public static final Supplier<CreativeModeTab> BLACK_OPAL_ITEMS_TAB =
+            CREATIVE_MODE_TAB.register("black_opal_items_tab", () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.tutorialmodrw.black_opal_items_tab"))
+                    .icon(() -> new ItemStack(ModItems.BLACK_OPAL.get()))
+                    .displayItems(((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.BLACK_OPAL);
+                        output.accept(ModItems.RAW_BLACK_OPAL);
+                    }))
+                    .build());
+
+    public static final Supplier<CreativeModeTab> BLACK_OPAL_BLOCKS_TAB =
+            CREATIVE_MODE_TAB.register("black_opal_blocks_tab", () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.tutorialmodrw.black_opal_blocks_tab"))
+                    .icon(() -> new ItemStack(ModItems.RAW_BLACK_OPAL.get()))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(MCCourseMod.MOD_ID,"black_opal_items_tab"))
+                    .displayItems(((itemDisplayParameters, output) -> {
+                        output.accept(Blocks.ANDESITE);
+                        output.accept(Blocks.DEEPSLATE_DIAMOND_ORE);
+                    }))
+                    .build());
+
+
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+}
