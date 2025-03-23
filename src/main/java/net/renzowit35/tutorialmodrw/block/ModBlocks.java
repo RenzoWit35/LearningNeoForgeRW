7b9a91623d28396d3f4a13d6cc6e66d9ee707747
Renzowit35 <Renzowitlox@gmail.com>
2025-03-23 16:33:34 +0100

    Patch 0.0.5 Ore Blocks
    Added Varients for ores and ores
    
    Added blocks to creative tab

--- src/main/java/net/renzowit35/tutorialmodrw/block/ModBlocks.java
+++ src/main/java/net/renzowit35/tutorialmodrw/block/ModBlocks.java
@@ -1,8 +1,10 @@
 package net.renzowit35.tutorialmodrw.block;
 
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
@@ -22,6 +24,22 @@ public class ModBlocks {
 
     public static final DeferredBlock<Block> RAW_BLACK_OPAL_BLOCK = registerblock("raw_black_opal_block",
             () -> new Block(BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops()));
+
+    public static final DeferredBlock<Block> BLACK_OPAL_ORE = registerblock("black_opal_ore",
+            () -> new DropExperienceBlock(UniformInt.of(2, 5),
+                    BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops()));
+
+    public static final DeferredBlock<Block> BLACK_OPAL_DEEPSLATE_ORE = registerblock("black_opal_deepslate_ore",
+            () -> new DropExperienceBlock(UniformInt.of(2, 5),
+                    BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops()));
+
+    public static final DeferredBlock<Block> BLACK_OPAL_NETHER_ORE = registerblock("black_opal_nether_ore",
+            () -> new DropExperienceBlock(UniformInt.of(2, 5),
+                    BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops()));
+
+    public static final DeferredBlock<Block> BLACK_OPAL_END_ORE = registerblock("black_opal_end_ore",
+            () -> new DropExperienceBlock(UniformInt.of(2, 5),
+                    BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops()));
 
 
 
